4ce42dc9eacaedaf66a8c752733408b8191214b5
Markus Koskimies <mkoskim@gmail.com>
2015-10-11 00:08:36 +0300

    Sketching target manager.

--- TargetManager/Main.lua
+++ TargetManager/Main.lua
@@ -37,6 +37,57 @@ local DefaultSettings = {
 
 local Settings = PlugIn:LoadSettings("TgtMgrSettings", DefaultSettings)
 
+-- ----------------------------------------------------------------------------
+
+local player = Turbine.Gameplay.LocalPlayer:GetInstance();
+
+-- ****************************************************************************
+
+TargetWidget = class(Turbine.UI.Control)
+
+function TargetWidget:Constructor()
+    Turbine.UI.Control.Constructor(self)
+
+    self:SetMouseVisible(false)
+
+    self.entity = Turbine.UI.Lotro.EntityControl()
+    self.entity:SetParent(self)
+    self.entity:SetSelectionEnabled(true)
+    self.entity:SetMouseVisible(true)
+    self.entity:SetBackColor(Turbine.UI.Color.Blue)
+    --[[
+    self.entity:SetContextMenuEnabled(true)
+    ]]--
+    self.entity.MouseClick = function()
+        DEBUG("Clicked!")
+    end
+
+    self.label = Turbine.UI.Label()
+    self.label:SetParent(self)
+    self.label:SetMouseVisible(false)
+    self.label:SetMultiline(false)
+
+    self:SetTarget(nil)
+end
+
+function TargetWidget:SizeChanged()
+    self.label:SetSize(self:GetWidth() - 20, self:GetHeight())
+    self.entity:SetSize(self:GetWidth() - 20, self:GetHeight())
+    --[[
+    self.entity:SetPosition(self:GetWidth() - 20, 0)
+    self.entity:SetSize(20, self:GetHeight())
+    ]]--
+end
+
+function TargetWidget:SetTarget(entity)
+    if entity ~= nil then
+        self.label:SetText(entity:GetName())
+    else
+        self.label:SetText("- - -")
+    end
+    self.entity:SetEntity(entity)
+end
+
 -- ****************************************************************************
 -- ****************************************************************************
 --
@@ -57,6 +108,23 @@ function MainWindow:Constructor()
 	self:SetText("Target Manager");
 
 	self:SetResizable(true);
+
+	-- ------------------------------------------------------------------------
+	-- 
+	-- ------------------------------------------------------------------------
+
+    self.target = TargetWidget()
+    self.target:SetParent(self)
+    self.target:SetPosition(20, 40)
+    self.target:SetSize(200, 20)
+
+    self.target.MouseClick = function(sender, args)
+        DEBUG("Clicked!")
+    end
+
+    -- player.TargetChanged = function(sender, args)
+        -- self.targetlabel:SetText(player:GetTarget():GetName())
+    -- end
 
 	-- ------------------------------------------------------------------------
 	-- Place window
@@ -136,12 +204,16 @@ atexit(function() mainwnd:Unload() end)
 local _cmd = Turbine.ShellCommand();
 
 function _cmd:Execute(cmd, args)
-	if ( args == "show" ) then
+	if (args == "show") then
 		mainwnd:SetVisible( true );
-	elseif ( args == "hide" ) then
+	elseif (args == "hide") then
 		mainwnd:SetVisible( false );
-	elseif ( args == "toggle" ) then
+	elseif (args == "toggle") then
 		mainwnd:SetVisible( not mainwnd:IsVisible() );
+    elseif (args == "store") then
+        mainwnd.target:SetTarget(player:GetTarget())
+    elseif (args == "pick") then
+        -- player:SetTarget(mainwnd.target.object)
 	else
 		INFO("/%s [show | hide | toggle]", cmd)
 	end
